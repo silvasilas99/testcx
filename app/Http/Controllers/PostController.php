d11050ecaa550a65561e114106e1ed1524e8cb82
Silas Silva <silvasilas66@gmail.com>
2021-05-12 21:34:02 -0300

    Criacao do controller e views que irao consumir a API

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Http::get('https://jsonplaceholder.typicode.com/posts');
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Http::get('https://jsonplaceholder.typicode.com/posts', [
+            'id' => $id,
+        ]);
+        return view('posts.show', compact('post'));
+    }
+
+}
